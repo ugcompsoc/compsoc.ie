26ae4ea107fd2c05a1e89bcfe808f46d114fb4e5
Shane Hastings <hastingsshane35@gmail.com>
2020-10-23 16:45:23 +0100

    Endpoint calls efficiency over 9000

--- includes/allEvents.sample.php
+++ includes/allEvents.sample.php
@@ -7,15 +7,55 @@
  */
 
 date_default_timezone_set('Europe/Dublin');
+
 // jsonEvents (all CompSoc events)
-$compsocEvents = "ENDPOINT_URL";
+$compsocEvents = "ENDPOINTURL";
 // jsonEvent (URL + $eventID)
-$eventDetail = "ENDPOINT_URL";
+$eventDetail = "ENDPOINTURL";
 
 // Import events from Socs Portal endpoint
 $allEventsImport = file_get_contents("$compsocEvents");
 $allEvents = json_decode($allEventsImport, true);
 
+/*
+ * Takes all evenings in $allEvents array and does a jsonEvent async curl request on each
+ */
+
+//create the multiple cURL handle
+$mh = curl_multi_init();
+$requests = array();
+$results = array();
+
+// init all requests
+foreach ($allEvents as $i => $e)
+{
+    $requests[$i] = curl_init();
+
+    curl_setopt($requests[$i], CURLOPT_URL, $eventDetail . $e['eventDetailsID']);
+    curl_setopt($requests[$i], CURLOPT_POST, false);
+    curl_setopt($requests[$i], CURLOPT_RETURNTRANSFER, true);
+
+    curl_multi_add_handle($mh, $requests[$i]);
+}
+
+$index=null;
+
+// Exec all requests
+do {
+    curl_multi_exec($mh,$index);
+} while($index > 0);
+
+// get content and remove handles
+foreach($requests as $k => $ch) {
+    $res = json_decode(curl_multi_getcontent($ch), true);
+    $allEvents[$k]['descriptionHTML'] = $res[0]['descriptionHTML'];
+    $allEvents[$k]['eventReadUrl'] = $res[0]['eventReadUrl'];
+
+    curl_multi_remove_handle($mh, $ch);
+}
+
+// close
+curl_multi_close($mh);
 
 /*
  * jsonEvent - get more details about a specific event based on the eventID returned from YourSpace.
@@ -31,7 +71,6 @@ function getEventDetails($eventID)
     return $eventDetails;
 }
 
-
 /*
  * Lists all events. Past, future, present.
  * This is actually used to list past events, so I'll have to change this at some point.
@@ -45,9 +84,9 @@ function listEvents()
         echo date("F j, Y", strtotime($events['start']));
         echo " | " . date("g:i a", strtotime($events['start'])) . " to " . date("g:i a", strtotime($events['end']));
 
-        echo "<br>" . html_entity_decode(getEventDetails($events['eventDetailsID'])[0]['descriptionHTML']) . "";
+        echo "<br>" . html_entity_decode($events['descriptionHTML']) . "";
 
-        echo "<a target='_blank' href='https://socs.nuigalway.ie/" . getEventDetails($events['eventDetailsID'])[0]['eventReadUrl'] . "'><span class=\"badge badge-primary\">View / Join event</span></a><br><hr>";
+        echo "<a target='_blank' href='https://socs.nuigalway.ie/" . $events['eventReadUrl'] . "'><span class=\"badge badge-primary\">View / Join event</span></a><br><hr>";
 
     }
 
@@ -76,8 +115,8 @@ function listUpcomingEvents()
             echo " | " . date("g:i a", strtotime($events['start'])) . " to " . date("g:i a", strtotime($events['end']));
 
             // html_entity_decode to actually parse and render the YourSpace HTML on the page for images/links.
-            echo "<br>" . html_entity_decode(getEventDetails($events['eventDetailsID'])[0]['descriptionHTML']) . "<br>";
-            echo "<a target='_blank' href='https://socs.nuigalway.ie/" . getEventDetails($events['eventDetailsID'])[0]['eventReadUrl'] . "'><span class=\"badge badge-primary\">View / Join event</span></a><br>";
+            echo "<br>" . html_entity_decode($events['descriptionHTML']) . "<br>";
+            echo "<a target='_blank' href='https://socs.nuigalway.ie/" . $events['eventReadUrl'] . "'><span class=\"badge badge-primary\">View / Join event</span></a><br>";
         } else {
 
             // No upcoming events
