5395b26ec182a050b2602c730a5530ab7198c7e2
McGovMan <c.mcgovern15@nuigalway.ie>
2021-10-12 00:24:56 +0100

    adding ciarans pic

--- committee/index.php
+++ committee/index.php
@@ -173,7 +173,7 @@
                                 <!-- Row -->
                                 <div class="row">
                                     <div class="col-md-12 text-center">
-                                        <img src="/assets/img/committee/2021-22/default.jpg" alt="wrapkit"
+                                        <img src="/assets/img/committee/2021-22/ciaranmccarthy.jpg" alt="wrapkit"
                                             class="img-fluid rounded-circle"/>
                                     </div>
                                     <div class="col-md-12 text-center">
@@ -181,7 +181,7 @@
                                             <h5 class="mt-4 font-weight-medium mb-0">Ciarán McCarthy</h5>
                                             <h6 class="subtitle mb-3">Auditor</h6>
                                             <p>
-                                            <div class="text-left">This person is so ominous, we don't know much else beyond their name and rank. But maybe soon....</div>
+                                            <div class="text-left">CONTENT DUE IN THE MORNING</div>
                                             </p>
                                             <ul class="list-inline">
 
