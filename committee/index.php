79b2ce140f415261a68e2918f602b05b647e55be
McGovMan <c.mcgovern15@nuigalway.ie>
2021-10-12 00:06:27 +0100

    add ciaras content

--- committee/index.php
+++ committee/index.php
@@ -396,7 +396,7 @@
                                 <!-- Row -->
                                 <div class="row">
                                     <div class="col-md-12 text-center">
-                                        <img src="/assets/img/committee/2021-22/default.jpg" alt="wrapkit"
+                                        <img src="/assets/img/committee/2021-22/ciaragalvin.jpg" alt="wrapkit"
                                             class="img-fluid rounded-circle"/>
                                     </div>
                                     <div class="col-md-12 text-center">
@@ -404,7 +404,7 @@
                                             <h5 class="mt-4 font-weight-medium mb-0">Ciara Galvin</h5>
                                             <h6 class="subtitle mb-3">Public Relations Officer</h6>
                                             <p>
-                                            <div class="text-left">This person is so ominous, we don't know much else beyond their name and rank. But maybe soon....</div>
+                                            <div class="text-left">Ciara is in 4th of year computer science & IT | Drinks 10 cups of tea a day | Up the Kingdom</div>
                                             </p>
                                             <ul class="list-inline">
     
