9fd64fd279e898ea203c4fa3c74c0b63afa77784
McGovMan <c.mcgovern15@nuigalway.ie>
2021-10-18 15:10:59 +0100

    adding aoifes content

--- committee/index.php
+++ committee/index.php
@@ -424,29 +424,6 @@
                                     </div>
                                     <div class="col-md-12 text-center">
                                         <div class="pt-2">
-                                            <h5 class="mt-4 font-weight-medium mb-0">Tara Parkinson</h5>
-                                            <h6 class="subtitle mb-3">Ordinary Committee Member</h6>
-                                            <p>
-                                            <div class="text-left">This person is so ominous, we don't know much else beyond their name and rank. But maybe soon....</div>
-                                            </p>
-                                            <ul class="list-inline">
-        
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Row -->
-                            </div>
-
-                            <div class="col-lg-3 mb-4">
-                                <!-- Row -->
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <img src="/assets/img/committee/2021-22/default.jpg" alt="wrapkit"
-                                            class="img-fluid rounded-circle"/>
-                                    </div>
-                                    <div class="col-md-12 text-center">
-                                        <div class="pt-2">
                                             <h5 class="mt-4 font-weight-medium mb-0">Adam McPadden</h5>
                                             <h6 class="subtitle mb-3">Ordinary Committee Member</h6>
                                             <p>
@@ -488,7 +465,7 @@
                                 <!-- Row -->
                                 <div class="row">
                                     <div class="col-md-12 text-center">
-                                        <img src="/assets/img/committee/2021-22/default.jpg" alt="wrapkit"
+                                        <img src="/assets/img/committee/2021-22/aoifeoloughlin.jpg" alt="wrapkit"
                                             class="img-fluid rounded-circle"/>
                                     </div>
                                     <div class="col-md-12 text-center">
@@ -496,7 +473,7 @@
                                             <h5 class="mt-4 font-weight-medium mb-0">Aoife O'Loughlin</h5>
                                             <h6 class="subtitle mb-3">Ordinary Committee Member</h6>
                                             <p>
-                                            <div class="text-left">This person is so ominous, we don't know much else beyond their name and rank. But maybe soon....</div>
+                                            <div class="text-left">Hi! I'm Aoife and an OCM! I'm a fourth year CS and IT student. I joined CompSoc because why not? And you'll mostly likely find me in the CompSoc Minecraft server building hogwarts :)</div>
                                             </p>
                                             <ul class="list-inline">
         
