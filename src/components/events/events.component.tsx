22545b22f924e68786854c3d4b8138ad6e841c2f
Conor Mc Govern <conor@mcgov.ie>
2022-09-25 21:52:44 +0100

    adding events service

--- src/components/events/events.component.tsx
+++ src/components/events/events.component.tsx
@@ -1,6 +1,56 @@
-import { getEvents } from '../../services/events';
+import { useEffect, useState, createElement } from 'react';
+import { getEvents, AllEventsType, EventType } from '../../services/events';
+import Spinner from 'react-bootstrap/Spinner';
 
 const EventsComponent = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [allEvents, setAllEvents] = useState<AllEventsType>({} as AllEventsType);
+
+  
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                var events = await getEvents()
+                setAllEvents(events)
+                setIsLoading(false)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        getData()
+    }, []);
+
+    const renderEvent = (e: EventType) => {
+        const htmlDecode = ((content: string) => {
+            let e = document.createElement('div');
+            e.innerHTML = content;
+            return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+        })
+
+        // Using dangerouslySetInnerHTML as we know the data has been sanitised
+        return (
+            <div key={e.eventID + e.eventDetailsID}>
+                <h3>{e.title}</h3>
+                {<div dangerouslySetInnerHTML={{ __html: htmlDecode(e.startDateTimeFormatted) as string }} />}
+
+                <br></br>
+
+                {<div dangerouslySetInnerHTML={{ __html: htmlDecode(e.descriptionHTML) as string }} />}
+
+                <div className="btn-toolbar">
+                    <a target="_blank" href={e.eventReadUrl}>
+                        <button type="button" className="btn btn-success">View / Join event</button>
+                    </a>
+
+                    &nbsp;
+
+                    <button type="button" className="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Add to calendar</button>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
         <section id="about1" className="about section-bg">
@@ -15,7 +65,21 @@ const EventsComponent = () => {
                     </h5>
                     <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                         <div className="card-body">
-                            
+                            {
+                                isLoading ? <Spinner animation="border" /> : (
+                                    <>
+                                    {
+                                        allEvents?.upcoming.map((e: EventType, i) => {
+                                            return (renderEvent(e))
+                                            // TODO Want to put space after each event, need better solution
+                                            {
+                                                return (<><br></br></>)
+                                            }
+                                        })
+                                    }
+                                    </>
+                                )
+                            }
                         </div>
                     </div>
 
@@ -29,10 +93,19 @@ const EventsComponent = () => {
 
                     <div id="collapseTwo" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                         <div className="card-body">
-                                
+                        {
+                                isLoading ? <Spinner animation="border" /> : (
+                                    <>
+                                    {
+                                        allEvents?.past.map((e: EventType, i) => {
+                                            return (renderEvent(e))
+                                        })
+                                    }
+                                    </>
+                                )
+                            }
                         </div>
                     </div>
-
 
                 </div>
 
--- src/services/events.tsx
+++ src/services/events.tsx
@@ -1,20 +1,62 @@
+export interface EventType {
+    eventDetailsID:         number
+	eventID:                number
+	title:                  string
+	descriptionHTML:        string
+	description:            string
+	eventTypeTitle:         string
+	start:                  string
+	end:                    string
+	locationDetails:        string
+	startDateTimeFormatted: string
+	ownerID:                number
+	ownerTitle:             string
+	allDay:                 boolean
+	eventLocationGroupID:   number
+	tags:                   string
+	locationTypeTitle:      string
+	statusTypeTitle:        string
+	signUpUrl:              string
+	icon:                   string
+	eventLocationType:      string
+	className:              string
+	eventUrl:               string
+	eventReadUrl:           string
+	eventICalUrl:           string
+}
 
-// Makes a request to the API to get all events and saves all events locally for further tweaking (seperating between old and new)
-const getAllEvents = (() => {
-    
-})
+export interface AllEventsType {
+    past: Array<EventType>
+    upcoming: Array<EventType>
+}
 
 // Returns all events that have an end time after the current time
-const getPastEvents = ((lowerBound: number, upperBound: number) => {
-
+const getPastEvents = (async () => {
+    try {
+        // Todo need to change the domain
+        const response = await fetch('http://localhost:8080/v1/events/past/30')
+        const responseJson = await response.json()
+        return responseJson["data"]
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 // Returns all events that have an end time before the current time
-const getUpcomingEvents = ((owerBound: number, upperBound: number) => {
-
+const getUpcomingEvents = (async () => {
+    try {
+        const response = await fetch('http://localhost:8080/v1/events/upcoming/30')
+        const responseJson = await response.json()
+        return responseJson["data"]
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 // Returns all events seperated into two arrays, past and upcoming
-export const getEvents = (() => {
-
+export const getEvents = (async () => {
+    var events = {} as AllEventsType
+    events.past = await getPastEvents()
+    events.upcoming = await getUpcomingEvents()
+    return events
 })
