2fc5f34fe3f6d5f7855dca68858ab8cddb2af8ed
Seán K <sean@override.online>
2023-04-02 20:29:29 +0100

    Update Twitter Link to reflect new username
    This commit updates the Twitter link to reflect our username change from when the University of Galway changed its name.
    https://twitter.com/ugcompsoc

--- src/components/sidepanel/profilesection/profilesection.component.tsx
+++ src/components/sidepanel/profilesection/profilesection.component.tsx
@@ -4,7 +4,7 @@ const ProfileSectionComponent = () => {
             <img src="/assets/img/apple-touch-icon.png" alt="" className="img-fluid"></img>
             <h1 className="text-light"><a href="/">CompSoc<br></br>NUI Galway</a></h1>
             <div className="social-links mt-3 text-center">
-                <a href="https://twitter.com/NUIGCompsoc" className="twitter"><i className="bx bxl-twitter"></i></a>
+                <a href="https://twitter.com/ugcompsoc" className="twitter"><i className="bx bxl-twitter"></i></a>
                 <a href="https://www.facebook.com/nuigcompsoc/" className="facebook"><i className="bx bxl-facebook"></i></a>
                 <a href="https://www.linkedin.com/company/nuig-compsoc?originalSubdomain=ie" className="linkedin"><i className="bx bxl-linkedin"></i></a>
                 <a href="https://discord.compsoc.ie" className="discord"><i className="bx bxl-discord"></i></a>
