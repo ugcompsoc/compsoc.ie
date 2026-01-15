68f2418b7ec34ddbfc8dfb2d83c99d202ada0301
semyonfox <semyon.fox@gmail.com>
2026-01-15 15:14:59 +0000

    updated for ctf 2026
    made non breaking package update too

--- src/components/ctf/SponsorTable.component.tsx
+++ src/components/ctf/SponsorTable.component.tsx
@@ -13,19 +13,10 @@ const SponsorTable = () => {
           <td>Gold Sponsors</td>
         </tr>
         <tr>
-          <td className="centered">
-            <img src="/assets/img/ctf/genesys/Genesys Logo_Digital Vector-Full Color.svg" alt="Genesys Logo" style={{ maxWidth: '270px' }}></img>
-            <img src="/assets/img/ctf/cisco/Cisco_idmG3-3gIB_1.svg" alt="Cisco Logo" style={{ maxWidth: '215px' }}></img>
-          </td>
-        </tr>
-        <tr className="sponsor-level">
-          <td>Silver Sponsors</td>
-        </tr>
-        <tr>
             <td className="centered">
-            <img src="/assets/img/ctf/siren/blue-trademarked-siren-powering-investigations-logo.png" alt="Siren Logo" style={{ maxWidth: '200px' }}></img>
-            <img src="/assets/img/ctf/libertyIT/Liberty IT_idv6-YWeb1_1.svg" alt="LibertyIT Logo" style={{ maxWidth: '190px' }}></img>
-            <img src="/assets/img/ctf/centripetal/Centripetal_Blue (2).svg" alt="Centripetal Logo" style={{ maxWidth: '220px' }}></img>
+                <img src="/assets/img/ctf/centripetal/Centripetal_Blue (2).svg" alt="Centripetal Logo" style={{ maxWidth: '220px' }}></img>
+                <img src="/assets/img/ctf/libertyIT/Liberty IT_idv6-YWeb1_1.svg" alt="LibertyIT Logo" style={{ maxWidth: '190px' }}></img>
+                <img src="/assets/img/ctf/siren/blue-trademarked-siren-powering-investigations-logo.png" alt="Siren Logo" style={{ maxWidth: '200px' }}></img>
             </td>
         </tr>
       </tbody>
--- src/components/ctf/ctf.component.tsx
+++ src/components/ctf/ctf.component.tsx
@@ -8,8 +8,8 @@ const CTFComponent = () => {
         <div className="container">
             <section id="main-content" className="about">
                 <div className="hero-container">
-                    <h1>CompSoc Intervarsity Capture the Flag - 2025</h1>
-                    <img src="/assets/img/ctf/Medium_Res_CompSoc_CTF_Poster.jpg" alt="Floor Debugging" style={{maxHeight: "80vh"}}></img>
+                    <h1>CompSoc Intervarsity Capture the Flag - 2026</h1>
+                    <img src="/assets/img/ctf/CTF 2026 png.png" alt="Floor Debugging" style={{maxHeight: "80vh"}}></img>
                 </div>
 
                 <div className="section-title">
@@ -22,10 +22,10 @@ const CTFComponent = () => {
                 <div className="section-title">
                     <h2>Event Details</h2>
                     <ul>
-                        <li><strong>Date:</strong> February 1st, 2025</li>
+                        <li><strong>Date:</strong> February 7th, 2026</li>
                         <li><strong>Location:</strong> <a href='https://maps.app.goo.gl/E5cNa39RbYFtHbZ7A' target="_blank" rel="noopener noreferrer">Bailey Allen Hall, University of Galway</a></li>
                         <li><strong>Teams:</strong> Up to 4 members per team</li>
-                        <li><strong>Tickets:</strong> <a href="https://socs.universityofgalway.ie/shop/?id=NzM3" target="_blank" rel="noopener noreferrer">Available here</a></li>
+                        <li><strong>Tickets:</strong> <a href="https://socs.universityofgalway.ie/shop/?id=MTQyNg==" target="_blank" rel="noopener noreferrer">Available here</a></li>
                         <li><strong>Event Discord:</strong> <a href='https://ctf.compsoc.ie/discord' target="_blank" rel="noopener noreferrer">Join here!</a></li>
                     </ul>
                 </div>
@@ -73,11 +73,11 @@ const CTFComponent = () => {
                 <div className="section-title">
                     <h2>How do I sign up?</h2>
                     <p>
-                        You can buy your tickets at our college's <a href="https://socs.universityofgalway.ie/shop/?id=NzM3" target="_blank" rel="noopener noreferrer">society store</a>.
+                        You can buy your tickets at our college's <a href="https://socs.universityofgalway.ie/shop/?id=MTQyNg==" target="_blank" rel="noopener noreferrer">society store</a>.
                         <br />
                         Teams can be <em>up to four</em> members.<br />
                         To qualify for the inter-varsity leaderboard and prizes your team must all be from the same college.
-                        Don’t worry though, there will also be an open category for other teams to compete in against one another, which is open to non-students too!
+                        {/*Don’t worry though, there will also be an open category for other teams to compete in against one another, which is open to non-students too!*/}
                     </p>
                 </div>
 
@@ -99,11 +99,18 @@ const CTFComponent = () => {
                 <div className="section-title">
                     <h2>FAQs</h2>
 
+                    {/*<div className="faq-item">*/}
+                    {/*    <h4><strong>What can I do for food?</strong></h4>*/}
+                    {/*    <p>We’ll have a hot lunch from Gourmet Food Parlour (and some other treats!), with both meat, vegetarian and gluten free options available.<br />*/}
+                    {/*    <strong>Note: if you purchase your ticket after midnight, Wednesday the 29th, while we endeavour to have food, we cannot guarentee there is a sufficient surplus in the catering order for your ticket.</strong><br />*/}
+                    {/*    There is a Tesco and a Centra open nearby, alongside lots of other food outlets if something else takes your fancy.</p>*/}
+                    {/*</div>*/}
+
+
                     <div className="faq-item">
                         <h4><strong>What can I do for food?</strong></h4>
-                        <p>We’ll have a hot lunch from Gourmet Food Parlour (and some other treats!), with both meat, vegetarian and gluten free options available.<br />
-                        <strong>Note: if you purchase your ticket after midnight, Wednesday the 29th, while we endeavour to have food, we cannot guarentee there is a sufficient surplus in the catering order for your ticket.</strong><br />
-                        There is a Tesco and a Centra open nearby, alongside lots of other food outlets if something else takes your fancy.</p>
+                        <p>We’ll have a hot lunch and some other treats, with both meat, vegetarian and gluten free options available.<br />
+                            There is a Tesco and a Centra open nearby, alongside lots of other food outlets if something else takes your fancy.</p>
                     </div>
 
                     <div className="faq-item">
