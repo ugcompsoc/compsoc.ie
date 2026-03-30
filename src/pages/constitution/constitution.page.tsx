28d06249f79ea6ce5a9c82a29de20604a5be10f1
Technological-Anachronism <fitzpatrickcaoilfhionn@gmail.com>
2026-03-30 15:16:47 +0100

    Update constitution.page.tsx date of ratification

--- src/pages/constitution/constitution.page.tsx
+++ src/pages/constitution/constitution.page.tsx
@@ -9,7 +9,7 @@ const ConstitutionPage = () => {
                 <div className="d-flex justify-content-between align-items-center">
                     <h2>Constitution </h2>
                     <ol>
-                        Formally ratified by the USCG on the 27th May, 2022
+                        Formally ratified by the USCG on the 7th November, 2022
                     </ol>
                 </div>
 
