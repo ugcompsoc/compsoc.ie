56a202fe86805939479f1f89b10370bd06473beb
Semyon Fox <semyon.fox@gmail.com>
2026-07-25 23:43:54 +0100

    Patch critical dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import babel from "@rolldown/plugin-babel"
 import tailwindcss from "@tailwindcss/vite"
 import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import viteReact, {
-	reactCompilerPreset,
-} from "@vitejs/plugin-react"
+import viteReact from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { imagetools } from "vite-imagetools"
 
@@ -25,9 +22,6 @@ const config = defineConfig({
 			},
 		}),
 		viteReact(),
-		babel({
-			presets: [reactCompilerPreset()],
-		}),
 	],
 })
 
